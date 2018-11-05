8cae982a050807060c50bae12254a6250804d1bb
Swifty Wang <swiftywang@m800.com>
2018-11-05 11:54:53 +0800

    add ffmpeg 3.2.4 as git submodule

--- asciimediaconverter/src/main/jni/Android.mk
+++ asciimediaconverter/src/main/jni/Android.mk
@@ -6,7 +6,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ffmpeginvoke
 LOCAL_SRC_FILES := com_swifty_asciimediaconverter_jni_FFmpegKit.c ffmpeg.c ffmpeg_opt.c cmdutils.c ffmpeg_filter.c
-LOCAL_C_INCLUDES := /Users/swiftywang/project/ffmpeg-3.2.4/
+LOCAL_C_INCLUDES := FFmpeg-3.2.4/
 LOCAL_LDLIBS := -llog -lz -ldl
 LOCAL_SHARED_LIBRARIES := ffmpeg
 include $(BUILD_SHARED_LIBRARY)
